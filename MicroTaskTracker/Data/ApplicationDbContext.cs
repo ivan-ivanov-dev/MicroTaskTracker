2c8a566950e1c9be4fc76c34b9a17865342def5c
MagMen1008 <darkkey7712@gmail.com>
2026-01-25 15:48:29 +0200

    Add ApplicationDbContext with DbSet<TaskItem>.

---
+++ MicroTaskTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MicroTaskTracker.Models;
+
+namespace MicroTaskTracker.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+        public DbSet<TaskItem> Tasks { get; set; } = null!;
+    }
+}
