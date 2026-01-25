4d73130950da2ddd950cc51271d5a80b5e849a85
MagMen1008 <darkkey7712@gmail.com>
2026-01-25 15:40:12 +0200

    Creating the TaskItemModel.

---
+++ MicroTaskTracker/Models/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MicroTaskTracker.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = null!;
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreatedOn{ get; set; }
+
+        public DateTime? DueDate { get; set; }
+    }
+}
