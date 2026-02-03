8fa24680dd66ea6d7b921361af81d1fdfb29b0f4
MagMen1008 <darkkey7712@gmail.com>
2026-02-03 20:46:37 +0200

    Add sign in and login view models

---
+++ MicroTaskTracker/Models/ViewModels/Authentication/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MicroTaskTracker.Models.ViewModels.Authentication
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; } = null!;
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ MicroTaskTracker/Models/ViewModels/Authentication/SignInViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MicroTaskTracker.Models.ViewModels.Authentication
+{
+    public class SignInViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
