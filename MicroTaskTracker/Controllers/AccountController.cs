57ea55ec368da5edf4e048c6836775ed2527cd96
MagMen1008 <darkkey7712@gmail.com>
2026-02-03 21:13:22 +0200

    Add AccountController

---
+++ MicroTaskTracker/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MicroTaskTracker.Models.ViewModels.Authentication;
+
+namespace MicroTaskTracker.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register(SignInViewModel signInViewModel)
+        {
+            // Registration logic here
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(SignInViewModel signInViewModel,string email, string password)
+        {
+            // Registration logic here
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            // Login logic here
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login(LoginViewModel loginViewModel,string email, string password)
+        {
+            // Login logic here
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            // Logout logic here
+            return View();
+        }
+    }
+}
--- MicroTaskTracker/Controllers/TasksController.cs
+++ MicroTaskTracker/Controllers/TasksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MicroTaskTracker.Data;
 using MicroTaskTracker.Models.DBModels;
