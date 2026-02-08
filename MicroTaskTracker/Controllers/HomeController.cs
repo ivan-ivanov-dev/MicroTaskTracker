c101acd1830f20596df2bee64b98e9f25c21e9e3
MagMen1008 <darkkey7712@gmail.com>
2026-02-08 18:47:27 +0200

    Add sidebar when user is logged in

--- MicroTaskTracker/Controllers/HomeController.cs
+++ MicroTaskTracker/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MicroTaskTracker.Models.ViewModels;
 namespace MicroTaskTracker.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
