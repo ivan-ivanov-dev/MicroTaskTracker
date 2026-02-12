de9003f70d25a292f4ba84fc060e0643e4e609ed
MagMen1008 <darkkey7712@gmail.com>
2026-02-12 20:09:37 +0200

    Add Action DB Model

---
+++ MicroTaskTracker/Models/DBModels/ActionItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MicroTaskTracker.Models.DBModels
+{
+    public class ActionItem
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+        public string Title { get; set; } = null!;
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; } = false;
+
+        [Required]
+        public string UserId { get; set; } = null!;
+        public ApplicationUser User { get; set; } = null!;
+
+        [Required]
+        public int GoalId { get; set; }
+        public Goal Goal { get; set; } = null!;
+
+        public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+    }
+}
