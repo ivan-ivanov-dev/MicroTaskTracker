58e37c030bf12089fa0e6da44ab5f3413b6e743a
MagMen1008 <darkkey7712@gmail.com>
2026-02-12 20:15:36 +0200

    Adjust realationships between goals, actions and tasks in the application db context and add db sets for goals and actions

--- MicroTaskTracker/Data/ApplicationDbContext.cs
+++ MicroTaskTracker/Data/ApplicationDbContext.cs
@@ -13,13 +13,15 @@ namespace MicroTaskTracker.Data
         public DbSet<TaskItem> Tasks => Set<TaskItem>();
         public DbSet<Tag> Tags => Set<Tag>();
         public DbSet<TaskTag> TaskTags => Set<TaskTag>();
+        public DbSet<Goal> Goals => Set<Goal>();
+        public DbSet<ActionItem> Actions => Set<ActionItem>();
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
             builder.Entity<TaskTag>()
-        .HasKey(tt => new { tt.TaskId, tt.TagId });
+                .HasKey(tt => new { tt.TaskId, tt.TagId });
 
             builder.Entity<TaskTag>()
                 .HasOne(tt => tt.Task)
@@ -36,6 +38,25 @@ namespace MicroTaskTracker.Data
                 .WithMany()
                 .HasForeignKey(t => t.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Goal>()
+                .HasOne(g => g.User)
+                .WithMany() // if you don’t need a collection in ApplicationUser
+                .HasForeignKey(g => g.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Goal>()
+                .HasMany(g => g.Actions)
+                .WithOne(a => a.Goal)
+                .HasForeignKey(a => a.GoalId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<ActionItem>()
+                .HasMany(a => a.Tasks)
+                .WithOne(t => t.Action)
+                .HasForeignKey(t => t.ActionId)
+                .OnDelete(DeleteBehavior.SetNull);
+
         }
     }
 }
