2526d27998524c9a9a2cc46b0e95bdd4abd04ed0
MagMen1008 <darkkey7712@gmail.com>
2026-02-12 17:24:14 +0200

    Adgust ApplicationDbContext so it saves data correctly

--- MicroTaskTracker/Data/ApplicationDbContext.cs
+++ MicroTaskTracker/Data/ApplicationDbContext.cs
@@ -11,5 +11,31 @@ namespace MicroTaskTracker.Data
         {
         }
         public DbSet<TaskItem> Tasks => Set<TaskItem>();
+        public DbSet<Tag> Tags => Set<Tag>();
+        public DbSet<TaskTag> TaskTags => Set<TaskTag>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<TaskTag>()
+        .HasKey(tt => new { tt.TaskId, tt.TagId });
+
+            builder.Entity<TaskTag>()
+                .HasOne(tt => tt.Task)
+                .WithMany(t => t.TaskTags)
+                .HasForeignKey(tt => tt.TaskId);
+
+            builder.Entity<TaskTag>()
+                .HasOne(tt => tt.Tag)
+                .WithMany(t => t.TaskTags)
+                .HasForeignKey(tt => tt.TagId);
+
+            builder.Entity<Tag>()
+                .HasOne(t => t.User)
+                .WithMany()
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
