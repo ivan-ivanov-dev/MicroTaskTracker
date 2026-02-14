46b45a4aa5ef0c43e35873343dfdc7ac429ff210
MagMen1008 <darkkey7712@gmail.com>
2026-02-14 22:01:24 +0200

    Move Data and DBModels to Pathly.Data and Pathly.DBModels in the solution folder - Data

--- Pathly.Data/ApplicationDbContext.cs
+++ Pathly.Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Pathly.Models.DBModels;
 
 namespace Pathly.Data
 {
-    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
